02ee23e5aa67e10814a9c2153825b55ddf999367
Andrea Cardaci <cyrus.and@gmail.com>
2014-06-30 03:50:15 +0200

    Add a minimal test suite

---
+++ test/methods.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var assert = require('assert');
+var synchttp = require('../');
+
+var port = 3000;
+
+function startServer(done) {
+    var server = http.createServer();
+    server.on('request', function (request, response) {
+        var incomingMessage;
+        var outgoingMessage = {};
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        switch (request.method) {
+        case 'GET':
+            outgoingMessage.path = request.url;
+            response.end(JSON.stringify(outgoingMessage));
+            break;
+        case 'POST':
+            request.on('readable', function () {
+                var body = request.read().toString();
+                incomingMessage = JSON.parse(body);
+                response.end(JSON.stringify(incomingMessage));
+            });
+            break;
+        }
+    });
+    server.listen(port, '127.0.0.1', function () {
+        done();
+    });
+}
+
+describe('HTTP methods', function () {
+    before(startServer);
+
+    it('GET', function (done) {
+        synchttp(function (http) {
+            var path = '/foo/bar/baz';
+            var response = http.get('http://localhost:' + port + path);
+            assert(response.path === path);
+            done();
+        });
+    });
+
+    it('POST', function (done) {
+        synchttp(function (http) {
+            var message = {
+                'foo': 1,
+                'bar': 2,
+                'baz': 3
+            };
+            var response = http.post('http://localhost:' + port, message);
+            assert(JSON.stringify(response) === JSON.stringify(message));
+            done();
+        });
+    });
+});
