f4478cc8b788afd3a270d23b9df84e92af03008e
Andrea Cardaci <cyrus.and@gmail.com>
2014-06-30 13:27:18 +0200

    Add code comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Fiber = require('fibers');
 var http = require('./lib/http.js');
 
+// run the supplied code within a fiber
 module.exports = function (instructions) {
     Fiber(function () {
         instructions(http);
