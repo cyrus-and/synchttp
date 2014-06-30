c27d29e915628d3361282fd447c6bec46dcaf8fe
Andrea Cardaci <cyrus.and@gmail.com>
2014-06-30 19:09:17 +0200

    Refactor test suite

--- test/methods.js
+++ test/methods.js
@@ -7,22 +7,10 @@ var port = 3000;
 function startServer(done) {
     var server = http.createServer();
     server.on('request', function (request, response) {
-        var incomingMessage;
-        var outgoingMessage = {};
+        var message = {};
         response.writeHead(200, {'Content-Type': 'application/json'});
-        switch (request.method) {
-        case 'GET':
-            outgoingMessage.path = request.url;
-            response.end(JSON.stringify(outgoingMessage));
-            break;
-        case 'POST':
-            request.on('readable', function () {
-                var body = request.read().toString();
-                incomingMessage = JSON.parse(body);
-                response.end(JSON.stringify(incomingMessage));
-            });
-            break;
-        }
+        message.method = request.method;
+        response.end(JSON.stringify(message));
     });
     server.listen(port, '127.0.0.1', function () {
         done();
@@ -32,25 +20,13 @@ function startServer(done) {
 describe('HTTP methods', function () {
     before(startServer);
 
-    it('GET', function (done) {
-        synchttp(function (http) {
-            var path = '/foo/bar/baz';
-            var response = http.port(port).path(path).get();
-            assert(response.path === path);
-            done();
-        });
-    });
-
-    it('POST', function (done) {
-        synchttp(function (http) {
-            var message = {
-                'foo': 1,
-                'bar': 2,
-                'baz': 3
-            };
-            var response = http.port(port).post(message);
-            assert(JSON.stringify(response) === JSON.stringify(message));
-            done();
+    ['GET', 'POST', 'PUT', 'DELETE'].forEach(function (method) {
+        it(method, function (done) {
+            synchttp(function (http) {
+                var response = http.port(port)[method.toLowerCase()]();
+                assert.equal(response.method, method);
+                done();
+            });
         });
     });
 });
