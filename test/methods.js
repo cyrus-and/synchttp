0cb8535de429709f04de5737b331a7d54f9e18e0
Andrea Cardaci <cyrus.and@gmail.com>
2014-06-30 19:53:52 +0200

    Add some more test cases

--- test/methods.js
+++ test/methods.js
@@ -2,31 +2,160 @@ var http = require('http');
 var assert = require('assert');
 var synchttp = require('../');
 
-var port = 3000;
+var server;
 
-function startServer(done) {
-    var server = http.createServer();
+function startServer(port, done) {
+    server = http.createServer();
     server.on('request', function (request, response) {
         var message = {};
         response.writeHead(200, {'Content-Type': 'application/json'});
         message.method = request.method;
-        response.end(JSON.stringify(message));
+        message.contentType = request.headers['content-type'];
+        message.contentLength = request.headers['content-length'];
+        message.hostPort = request.headers['host'];
+        message.path = request.url;
+        message.headers = request.headers;
+        if (typeof message.contentLength !== 'undefined' && message.contentLength > 0) {
+            request.on('readable', function () {
+                message.body = request.read().toString();
+                response.end(JSON.stringify(message));
+            });
+
+        } else {
+            response.end(JSON.stringify(message));
+        }
     });
     server.listen(port, '127.0.0.1', function () {
         done();
     });
 }
 
+function stopServer() {
+    server.close();
+}
+
 describe('HTTP methods', function () {
-    before(startServer);
+    before(function (done) {
+        startServer(3000, done)
+    });
+    after(stopServer);
 
     ['GET', 'POST', 'PUT', 'DELETE'].forEach(function (method) {
         it(method, function (done) {
             synchttp(function (http) {
-                var response = http.port(port)[method.toLowerCase()]();
+                var response = http[method.toLowerCase()]();
                 assert.equal(response.method, method);
                 done();
             });
+        });
+    });
+});
+
+describe('Default parameters', function () {
+    before(function (done) {
+        startServer(3000, done)
+    });
+    after(stopServer);
+
+    it('with body', function (done) {
+        synchttp(function (http) {
+            var response = http.post('foo');
+            assert.equal(response.contentType, 'application/json');
+            assert.equal(response.contentLength, response.body.length);
+            assert.equal(response.hostPort, 'localhost:3000');
+            assert.equal(response.path, '/');
+            done();
+        });
+    });
+
+    it('without body', function (done) {
+        synchttp(function (http) {
+            var response = http.post();
+            assert.equal(response.contentType, undefined);
+            assert.equal(response.contentLength, 0);
+            assert.equal(response.hostPort, 'localhost:3000');
+            assert.equal(response.path, '/');
+            done();
+        });
+    });
+
+    it('for methods that do not allow the body', function (done) {
+        synchttp(function (http) {
+            var response = http.get();
+            assert.equal(response.contentType, undefined);
+            assert.equal(response.contentLength, undefined);
+            assert.equal(response.hostPort, 'localhost:3000');
+            assert.equal(response.path, '/');
+            done();
+        });
+    });
+});
+
+describe('Custom parameters', function () {
+    before(function (done) {
+        startServer(1337, done)
+    });
+    after(stopServer);
+
+    it('with body', function (done) {
+        synchttp(function (http) {
+            var response = http
+                .contentType('application/x-www-form-urlencoded')
+                .host('127.0.0.1')
+                .port('1337')
+                .path('/foo?bar=baz')
+                .headers({
+                    'foo': 'bar'
+                })
+                .post({
+                    'name': 'value'
+                });
+            assert.equal(response.contentType, 'application/x-www-form-urlencoded');
+            assert.equal(response.contentLength, response.body.length);
+            assert.equal(response.hostPort, '127.0.0.1:1337');
+            assert.equal(response.path, '/foo?bar=baz');
+            assert.equal(response.headers.foo, 'bar');
+            done();
+        });
+    });
+
+    it('without body', function (done) {
+        synchttp(function (http) {
+            var response = http
+                .contentType('application/x-www-form-urlencoded')
+                .host('127.0.0.1')
+                .port('1337')
+                .path('/foo?bar=baz')
+                .headers({
+                    'foo': 'bar'
+                })
+                .post();
+            assert.equal(response.contentType, undefined);
+            assert.equal(response.contentLength, 0);
+            assert.equal(response.hostPort, '127.0.0.1:1337');
+            assert.equal(response.path, '/foo?bar=baz');
+            assert.equal(response.headers.foo, 'bar');
+            done();
+        });
+    });
+
+    it('for methods that do not allow the body', function (done) {
+        synchttp(function (http) {
+            var response = http
+                .contentType('application/x-www-form-urlencoded')
+                .host('127.0.0.1')
+                .port('1337')
+                .path('/foo?bar=baz')
+                .headers({
+                    'foo': 'bar'
+                })
+                .get();
+            assert.equal(response.contentType, undefined);
+            assert.equal(response.contentLength, undefined);
+            assert.equal(response.hostPort, '127.0.0.1:1337');
+            assert.equal(response.path, '/foo?bar=baz');
+            assert.equal(response.headers.foo, 'bar');
+            done();
         });
     });
 });
