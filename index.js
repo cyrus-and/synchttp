5c13bc3eb4c8b47c0f5d62f1bfca8e57f3a4103f
Andrea Cardaci <cyrus.and@gmail.com>
2014-07-02 22:56:04 +0200

    Protect private members

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ var Synchttp = require('./lib/synchttp.js');
 module.exports = function (instructions) {
     Fiber(function () {
         var synchttp = new Synchttp();
-        instructions(synchttp);
+        var delegate = {};
+        // copy parameters
+        for (var parameter in synchttp.setters) {
+            delegate[parameter] = synchttp.setters[parameter];
+        }
+        // copy methods
+        delegate.get = synchttp.httpGet;
+        delegate.post = synchttp.httpPost;
+        delegate.put = synchttp.httpPut;
+        delegate.delete = synchttp.httpDelete;
+        delegate.ws = synchttp.wsConnect;
+        delegate.send = synchttp.wsSend;
+        delegate.receive = synchttp.wsReceive;
+        // run cliend code
+        instructions(delegate);
+        // terminate all the WebSockets
         synchttp.wsCloseAll();
     }).run();
 };
